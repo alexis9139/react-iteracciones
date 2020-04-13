9f79de93a6c8da5fbcf00c80f40bde9358bf629e
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-04-13 17:00:01 -0300

    Iterando listas con React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const frutas = [
+  'manzana',
+  'naranja',
+  'pera',
+  'kiwi',
+  'banana'
+]
+
+const numeros = [1, 2, 3, 4]
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          {frutas.map((fruta) => {
+            return (
+              <li>{fruta}</li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
