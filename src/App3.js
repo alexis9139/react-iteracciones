a4c609dc900c00d5118f51fc8bb0991abc0ffd8b
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-04-13 19:39:44 -0300

    Iterando propiedades de Objetos

---
+++ src/App3.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class App3 extends React.Component {
+    //inicializamos el estado
+    state = {
+        user: {
+            nombre: "Alexis",
+            pais: "Argentina",
+            youtube: "alexis.youtube.com|"
+        }
+    }
+    render() {
+        const { user } = this.state
+        const keys = Object.keys(user)//esto devuelve un array
+        //es decir [nombre,pais,youtube] de user
+        return (
+            <div>
+                <ul>
+                    {keys.map((key) => (
+                        <li><strong>{key}:</strong> {user[key]}</li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
+export default App3
